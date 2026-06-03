a676b25971a5348d9e4c2c04480eaa5b895cc6f9
Bench Tapic <onlineacademy23est@gmail.com>
2026-06-03 16:06:00 +0800

    Add SSL to database connection

--- server/server.js
+++ server/server.js
@@ -32,7 +32,8 @@ const pool = mysql.createPool({
     port: parseInt(process.env.DB_PORT) || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: { rejectUnauthorized: false }
 });
 
 // Test connection sa startup
